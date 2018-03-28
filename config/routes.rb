7346a1d9837b87d207b466ecc74b729e899e2c2a
EC2 Default User <ec2-user@ip-172-31-3-58.us-east-2.compute.internal>
2018-03-28 17:19:28 +0000

    Set the root view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'stock/index'
 
+  root 'stock#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
