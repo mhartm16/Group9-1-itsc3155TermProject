8689fe92fab955f8d92b1547666d4c845281772a
Michael Hartman <mhartm16@uncc.edu>
2018-03-29 22:30:13 +0000

    Replaced the old controller and recreated a new action and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'stock/index'
 
-  resources :account
+  resources :users
 
   root 'stock#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
