63adf821952b305810b85a5c27b625f36bcf1831
Michael Hartman <mhartm16@uncc.edu>
2018-03-29 21:49:39 +0000

    Created the new action and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'stock/index'
 
+  resources :account
+
   root 'stock#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
