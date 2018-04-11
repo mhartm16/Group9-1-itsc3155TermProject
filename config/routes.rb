58dc7b615dffe184452a4930f07e2845ad30e813
Michael Hartman <mhartm16@uncc.edu>
2018-04-11 21:23:40 +0000

    Add feedback routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'stock/index'
 
-  resources :users
+  resources :users do
+    resources :feedbacks
+  end
 
   root 'stock#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
