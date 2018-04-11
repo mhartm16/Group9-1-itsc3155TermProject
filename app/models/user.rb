3004c1c5d13c50ca94e0eebde0298cec1e0b564c
Michael Hartman <mhartm16@uncc.edu>
2018-04-11 21:12:43 +0000

    Added associated feedback model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :feedbacks
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true, length: { minimum: 5}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402164334) do
+ActiveRecord::Schema.define(version: 20180411210916) do
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "users_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_feedbacks_on_users_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
